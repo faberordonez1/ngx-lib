231d73bc40cc11031ff1a2f310c6273d2d0c6ce2
Faber Ordoñez <faberordonez1@gmail.com>
2023-08-06 21:29:42 -0500

    Create error component

---
+++ projects/ngx-shared-components/src/lib/components/error/error.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective } from '@angular/forms';
+
+@Component({
+  selector: 'fb-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css'],
+})
+export class ErrorComponent implements OnInit {
+  @Input() input!: FormControl;
+  @Input() form!: FormGroupDirective;
+
+  errorType: string = '';
+  errorTypeForm: string = '';
+
+  maxLength!: number;
+  minLength!: number;
+  max!: number;
+  min!: number;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.input.valueChanges.subscribe((_) => {
+      // if (this.input.errors?.minlength) {
+      //   this.minLength = this.input.errors.minlength.requiredLength;
+      // } else if (this.input.errors?.maxlength) {
+      //   this.maxLength = this.input.errors.maxlength.requiredLength;
+      // }
+
+      // if (this.input.errors?.min) {
+      //   this.min = this.input.errors.min.min;
+      // } else if (this.input.errors?.max) {
+      //   this.max = this.input.errors.max.max;
+      // }
+
+      if (this.input.invalid && (this.input.touched || this.input.dirty)) {
+        if (this.input.errors) {
+          this.errorType = Object.keys(this.input.errors)[0];
+        }
+      }
+      if (this.form.errors) {
+        this.errorTypeForm = Object.keys(this.form.errors)[0];
+      }
+    });
+  }
+
+  getCustomValidationLabel(errorType: string): string {
+    return errorType === '' ? errorType : `FORM_ERRORS.${errorType}`;
+  }
+}
--- projects/ngx-shared-components/src/lib/ngx-shared-components.module.ts
+++ projects/ngx-shared-components/src/lib/ngx-shared-components.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgxSharedComponentsComponent } from './ngx-shared-components.component';
-
-
+import { ErrorComponent } from './components/error/error.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    NgxSharedComponentsComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    NgxSharedComponentsComponent
-  ]
+  declarations: [NgxSharedComponentsComponent, ErrorComponent],
+  imports: [CommonModule],
+  exports: [NgxSharedComponentsComponent],
 })
-export class NgxSharedComponentsModule { }
+export class NgxSharedComponentsModule {}
